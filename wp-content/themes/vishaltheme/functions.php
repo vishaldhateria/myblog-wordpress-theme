b4bf1004eafa868e4fb26b3610434515f007c3da
vishal dhateria <vishaldhateria@gmail.com>
2018-02-28 18:57:56 +0530

    added sidebar

--- wp-content/themes/vishaltheme/functions.php
+++ wp-content/themes/vishaltheme/functions.php
@@ -48,10 +48,20 @@ Sidebar Function
 
 */
 
-function awesome_widget_setup(){
+function vishal_widget_setup(){
 
     register_sidebar(array(
-
+        array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar-1',
+            'class' => 'custom',
+            'description' => 'Standard Sidebar',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h1 class="widget-title">',
+            'after_title' => '</h1>',
+        )
     )
 );
 }
+add_action( 'widget_init', 'vishal_widget_setup');
